df37a1130de7c84f2018309c5fe8fa8ca7080188
Marcos <202202382@alu.comillas.com>
2025-05-11 13:54:26 +0200

    add average rating to ListCreate

--- myFirstApiRest/auctions/serializers.py
+++ myFirstApiRest/auctions/serializers.py
@@ -19,7 +19,7 @@ class AuctionListCreateSerializer(serializers.ModelSerializer):
     creation_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ",read_only=True)
     closing_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ")
     isOpen = serializers.SerializerMethodField(read_only=True)
-
+    average_rating = serializers.FloatField(read_only=True)
     class Meta:
         model = Auction
         fields = "__all__"
