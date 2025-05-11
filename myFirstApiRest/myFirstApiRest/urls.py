a2a98c0f8a1e7b6215029efab1e0941ac49cfe01
Marcos <202202382@alu.comillas.com>
2025-05-11 19:29:53 +0200

    try fixing images

--- myFirstApiRest/myFirstApiRest/urls.py
+++ myFirstApiRest/myFirstApiRest/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/auctions/', include('auctions.urls')),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
